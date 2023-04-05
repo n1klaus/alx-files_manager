40e1800d8695dca18079e6fe5bf92430a3d38ccf
Chalo1996 <emusyoka759@gmail.com>
2023-04-05 12:47:11 +0300

    Removed the Get /user/me Endpoint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,6 @@
 const sha1 = require('sha1');
 const { v4: uuidv4 } = require('uuid');
 const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -14,7 +13,7 @@ class UsersController {
     }
     const existingUser = await dbClient.getUser(email);
     if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
+      return res.status(400).json({ error: 'Already exists' });
     }
     const hashedPass = sha1(password);
     const newUser = {
@@ -24,20 +23,6 @@ class UsersController {
     };
     await dbClient.insertOneUser(newUser);
     return res.status(201).json({ id: newUser._id, email: newUser.email });
-  }
-
-  static async getMe(req, res) {
-    const token = req.headers['x-token'];
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const user = await dbClient.getUserById(userId);
-    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
