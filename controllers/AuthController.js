4e610a71b072583be441edeadfbbcbd436d53b58
Chalo1996 <emusyoka759@gmail.com>
2023-04-05 12:46:34 +0300

    Added the UserController class for the Get /user/me Endpoint

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -36,4 +36,20 @@ class AuthController {
   }
 }
 
-module.exports = AuthController;
+class UserController {
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const user = await dbClient.getUserById(userId);
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = { AuthController, UserController };
