715d24bdad4da73f6c2faebc2a954141f33e1457
Chalo1996 <emusyoka759@gmail.com>
2023-04-06 00:53:05 +0300

    routes Module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import AppController from '../controllers/AppController';
 import { UsersController, UserController } from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -14,5 +15,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
