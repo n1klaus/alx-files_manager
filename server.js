b9d6d918b3ebdde036443824afc87cb3acc76219
Emmanuel Chalo <emusyoka759@gmail.com>
2023-04-05 08:47:54 +0300

    server creation module

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const dbClient = require('./utils/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+(async () => {
+  if (await dbClient.isAlive()) {
+    console.log('MongoDB is connected');
+  } else {
+    console.log('MongoDB is not connected');
+  }
+})();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
